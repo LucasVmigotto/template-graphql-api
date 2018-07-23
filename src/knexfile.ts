ae5899fd8a50b2b9638aedf9f84c1802bbd1ffc5
Lucas Vidor Migotto <lmigotto@amix.com.br>
2018-07-23 17:37:11 -0300

    'DB_CLIENT adicionado ao .env.example, pequenas correções'

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -4,7 +4,7 @@ import * as path from 'path'
 dotenv.config({ path: path.join(__dirname, '..', '.env') })
 
 module.exports = {
-  client: 'pg',
+  client: process.env.DB_CLIENT,
   connection: process.env.DB_CONNECTION_STRING,
   migrations: {
     directory: './migrations'
